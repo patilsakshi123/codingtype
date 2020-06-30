d7af923db0ace0947fc4f1676a7807682c2cc43c
patilsakshi123 <67007239+patilsakshi123@users.noreply.github.com>
2020-06-30 08:30:43 +0530

    Add files via upload
    This program reverse the content of the file and copy content from one file to another.

---
+++ fileprogram.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+   char ch, source_file[20], target_file[20];
+   FILE *source, *target;
+   FILE *fp;
+   int i,pos;
+   
+   fp=fopen("newfile.txt","r");
+      if(fp==NULL)
+      {
+            printf("File does not exist..");
+      }
+      fseek(fp,0,SEEK_END);
+      pos=ftell(fp);
+      i=0;
+      while(i<pos)
+       {
+            i++;
+            fseek(fp,-i,SEEK_END);
+            ch=fgetc(fp);
+            printf("%c",ch);
+      }
+      printf("\nEnter name of file to copy\n");
+   gets(source_file);
+
+   source = fopen(source_file, "r");
+
+   if( source == NULL )
+   {
+      printf("Press any key to exit...\n");
+      exit(EXIT_FAILURE);
+   }
+
+   printf("Enter name of target file\n");
+   gets(target_file);
+
+   target = fopen(target_file, "w");
+
+   if( target == NULL )
+   {
+      fclose(source);
+      printf("Press any key to exit...\n");
+      exit(EXIT_FAILURE);
+   }
+
+   while( ( ch = fgetc(source) ) != EOF )
+      fputc(ch, target);
+
+   printf("File copied successfully.\n");
+
+   fclose(source);
+   fclose(target);
+   return 0;
+}
+
+
+
+
+
